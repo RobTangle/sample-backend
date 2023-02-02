ba14b3e65bc1af24cf8a42b93179570dc9b08ee2
Robert0101 <rob.tangled@gmail.com>
2023-02-01 22:33:50 -0300

    import and register microservices

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule } from '@nestjs/microservices';
+import { Transport } from '@nestjs/microservices/enums';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'COMMUNICATION',
+        transport: Transport.TCP,
+      },
+      {
+        name: 'ANALYTICS',
+        transport: Transport.TCP,
+        options: {
+          port: 3001,
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
